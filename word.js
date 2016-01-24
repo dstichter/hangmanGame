af4a3d745837bfb0e22aec3aaabaf1b38e004aa1
David Stichter <david.stichter3414@gmail.com>
2016-01-24 15:55:07 -0500

    Basic constructor functions done

--- word.js
+++ word.js
@@ -6,7 +6,25 @@ function Word(val){
   this.found = false;
   this.getLets = function(){
     for(var i = 0; i < this.word.length; i++){
-      this.letters.push()
+      this.letters.push(new Letter(this.word.charAt(i)))
     }
   }
+  this.checkIfLetterFound = function(guess){
+    for(var i = 0; i < this.letters.length; i++){
+      if(letters[i].value === guess){
+        letters.[i].appear = true;
+      }
+    }
+  }
+  this.didWeFindTheWord = this.letters.every(function (curLetter){
+    return curLetter.appear
+  });
+  this.wordRender = function() {
+    var str = ""
+    for(var i = 0; i < this.letters.length; i++){
+      str.concat(this.letters[i].letterRender)
+    }
+    return str
+  }
 }
+module.exports = Word;
